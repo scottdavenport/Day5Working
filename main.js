1c5539accd16612f6781896d13339ab7182c8c30
TarSoxJr <tarsox5@gmail.com>
2017-04-24 19:43:58 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var member = {
+  firstName:  "Scott",
+  lastName: "Davenport",
+  age: 41,
+  planType: "Dental Blue",
+  familyMember: {
+    sister: "Kathryn",
+    mother: "Kim",
+    father: "Josh"
+  }
+};
+
+console.log(member.familyMember);
